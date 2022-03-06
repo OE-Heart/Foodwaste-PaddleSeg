485c9a9cfd10f509f9026856983489cb87ccceca
OE-Heart <leoouyixin@126.com>
2022-03-07 00:58:52 +0800

    follow grayscale labeling protocol

--- work/partiton.py
+++ work/partiton.py
@@ -2,7 +2,7 @@ import os
 import cv2
 
 images_path = "../dataset/images"
-labels_path = "../dataset/labels"
+labels_path = "../dataset/annotations"
 
 train_file = open('../dataset/train_list.txt', 'w+')
 val_file = open('../dataset/val_list.txt', 'w+')
@@ -18,9 +18,9 @@ for path, dir_list, file_list in g:
             continue
 
         img_path = os.path.join(path, file_name)
-        img = cv2.imread(img_path, 1)
 
         if file_name[-5] == "中":
+            img = cv2.imread(img_path, 1)
             file_name = file_name.replace(file_name[-5], "", 1)
             new_Path = os.path.join(images_path, file_name)
 
@@ -34,6 +34,16 @@ for path, dir_list, file_list in g:
             cnt += 1
         
         if file_name[-5] == "右":
+            img = cv2.imread(img_path, 0)
+            img[img == 14] = 1
+            img[img == 33] = 2
+            img[img == 38] = 3
+            img[img == 52] = 4
+            img[img == 57] = 5
+            img[img == 75] = 6
+            img[img == 113] = 7
+            img[img == 128] = 8
+            img[img == 132] = 9
             file_name = file_name.replace(file_name[-5], "", 1)
             new_Path = os.path.join(labels_path, file_name)
 
